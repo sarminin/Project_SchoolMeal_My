b0a441c673d9483665d96a2e74f88059b90557b8
WOOBAELEE <le_domin@naver.com>
2024-11-04 16:48:15 +0900

    origin

---
+++ schoolMeal/src/main/java/com/example/schoolMeal/security/example.java
@@ -0,0 +1,5 @@
+package com.example.schoolMeal.security;
+
+public class example {
+
+}
---
+++ schoolMeal/src/test/java/com/example/schoolMeal/SchoolMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolMeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
